4c6fda8841d1207268470934d00ee5f85c75e00a
Andrew-Nekowitsch <anekowitsch@gmail.com>
2022-08-31 22:29:53 -0500

    web support

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import * as React from 'react'
 import { Text } from 'react-native'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import { Button, ThemeProvider } from '@rneui/themed'
 
 function Demo() {
 	return (
 		<SafeAreaView
-			style={{ flex: 1, justifyContent: 'space-between', alignItems: 'center', backgroundColor: 'red' }}>
+			style={{ flex: 1, justifyContent: 'space-between', alignItems: 'center', backgroundColor: 'grey' }}>
 			<Text>This is top text.</Text>
 			<Text>This is bottom text.</Text>
 		</SafeAreaView>
@@ -14,9 +15,14 @@ function Demo() {
 
 export default function App() {
 	return (
-		<SafeAreaProvider>
-			<Demo></Demo>
-		</SafeAreaProvider>
+		<>
+			<ThemeProvider>
+				<SafeAreaProvider>
+					<Demo></Demo>
+					<Button title='Hey!' />
+				</SafeAreaProvider>
+			</ThemeProvider>
+		</>
 	)
 }
 
